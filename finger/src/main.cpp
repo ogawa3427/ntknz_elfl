8ecdb3940c06fc702da609037f3f7b6c3177ad38
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2024-06-18 23:35:31 +0900

    Update main.cpp

--- finger/src/main.cpp
+++ finger/src/main.cpp
@@ -1,18 +1,49 @@
 #include <Arduino.h>
+#include <Wire.h>
+uint8_t goioNums[] = {0, 1, 2, 3, 6, 7, 8, 9, 10, 11, 12, 13};
 
-// put function declarations here:
-int myFunction(int, int);
+uint16_t dataX = 0; // ビット列として初期化
+uint16_t dataY = 0; // ビット列として初期化
+bool dataReadyX = false;  // データが準備完了したかどうかを示すフラグ
+bool dataReadyY = true;  // データが準備完了したかどうかを示すフラグ
+
+void requestEvent() {
+  Serial.println(dataX, BIN);
+  Serial.println(dataY, BIN);
+  if (dataReadyX) {
+    Wire.write((const uint8_t*)&dataX, sizeof(dataX));
+  } else if (dataReadyY) {
+    Wire.write((const uint8_t*)&dataY, sizeof(dataY));
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Wire.begin(0x08);
+  Wire.onRequest(requestEvent);
+  Serial.begin(9600);
+  pinMode(25, OUTPUT);
+  for (int i = 0; i < sizeof(goioNums)/sizeof(goioNums[0]); i++) {
+    pinMode(goioNums[i], INPUT_PULLUP);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (dataReadyX) {
+      dataY = 0; // dataYをリセット
+      for (int i = 0; i < sizeof(goioNums)/sizeof(goioNums[0]); i++) {
+        dataY |= digitalRead(goioNums[i]) == 0 ? (1 << i) : 0; // ビットをセット
+      }
+      dataY <<= 4; // 上位4ビットを0で埋める
+      dataReadyY = true;
+      dataReadyX = false;
+  } else if (dataReadyY) {
+    dataX = 0; // dataXをリセット
+    for (int i = 0; i < sizeof(goioNums)/sizeof(goioNums[0]); i++) {
+      dataX |= digitalRead(goioNums[i]) == 0 ? (1 << i) : 0; // ビットをセット
+    }
+    dataX <<= 4; // 上位4ビットを0で埋める
+    dataReadyX = true;
+    dataReadyY = false;
+  }
+  delay(10);
 }
